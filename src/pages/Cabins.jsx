423aa66c763df201cd0c4c76f06d2677edbd41bc
Shamilsharaf <165153833+Shamilsharaf@users.noreply.github.com>
2024-11-19 12:58:55 +0530

    Building the sidebar and navigation - re work

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,13 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+// src/pages/Cabins.jsx
+import React from "react";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+    return (
+        <div>
+            <h1>All Cabins</h1>
+            <p>TEST</p>
+        </div>
+    );
 }
 
 export default Cabins;

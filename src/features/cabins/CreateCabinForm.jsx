210dd3217990d7058c1619ca76aa57847f59f42a
Shamilsharaf <165153833+Shamilsharaf@users.noreply.github.com>
2024-11-27 19:36:43 +0530

    Building a Modal Window Using a React Portal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -37,11 +37,11 @@ const FormRow2 = styled.div`
   }
 `;
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useEditCabin();
 
-  const isWorking = isCreating || isEditing
+  const isWorking = isCreating || isEditing;
 
   const { id: editId, ...editValues } = cabinToEdit;
   const isEditSession = Boolean(editId);
@@ -52,22 +52,29 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   const { errors } = formState;
   console.log(errors);
 
-
   function onSubmit(data) {
-    const image = typeof data.image === 'string' ? data.image : data.image[0]
+    const image = typeof data.image === "string" ? data.image : data.image[0];
 
-    if (isEditSession) editCabin({ newCabinData: { ...data, image }, id: editId }, {
-      onSuccess: (data) => {
-        reset();
-      },
-    });
-
-    else createCabin({ ...data, image: data.image[0] }, {
-      onSuccess: (data) => {
-        reset();
-      },
-    }
-    );
+    if (isEditSession)
+      editCabin(
+        { newCabinData: { ...data, image }, id: editId },
+        {
+          onSuccess: (data) => {
+            reset();
+            onCloseModal?.();
+          },
+        }
+      );
+    else
+      createCabin(
+        { ...data, image: image },
+        {
+          onSuccess: (data) => {
+            reset();
+            onCloseModal?.();
+          },
+        }
+      );
   }
 
   function onError(errors) {
@@ -75,74 +82,103 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? "modal" : "regular"}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
-        <Input type="text" id="name" disabled={isWorking} {...register("name",
-          {
+        <Input
+          type="text"
+          id="name"
+          disabled={isWorking}
+          {...register("name", {
             required: "This field is required",
           })}
         />
-
       </FormRow>
 
-      <FormRow label='Maximum capacity' error={errors?.maxCapacity?.message}>
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
         {/* <Label htmlFor="maxCapacity">Maximum capacity</Label> */}
-        <Input type="number" id="maxCapacity" disabled={isWorking} {...register("maxCapacity",
-          {
+        <Input
+          type="number"
+          id="maxCapacity"
+          disabled={isWorking}
+          {...register("maxCapacity", {
             required: "This field is required",
             min: {
               value: 1,
               message: "Capacity should be at least 1",
             },
-          })} />
-
-
+          })}
+        />
       </FormRow>
 
-      <FormRow label='regularPrice' error={errors?.regularPrice?.message}>
-        <Input type="number" id="regularPrice" disabled={isWorking} {...register("regularPrice",
-          {
+      <FormRow label="regularPrice" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          disabled={isWorking}
+          {...register("regularPrice", {
             required: "This field is required",
             min: {
               value: 1,
               message: "Capacity should be at least 1",
             },
-          })} />
+          })}
+        />
       </FormRow>
 
-      <FormRow label='discount' error={errors?.discount?.message}>
-        <Input type="number" id="discount" disabled={isWorking} defaultValue={0} {...register("discount",
-          {
+      <FormRow label="discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          disabled={isWorking}
+          defaultValue={0}
+          {...register("discount", {
             required: "This field is required",
             validate: (value) =>
-              value <= +getValues().regularPrice || "Discount should be less than regular price",
-          })} />
+              value <= +getValues().regularPrice ||
+              "Discount should be less than regular price",
+          })}
+        />
       </FormRow>
 
-      <FormRow label='description' error={errors?.description?.message}>
-        <Textarea type="number" id="description" disabled={isWorking} defaultValue="" {...register("description",
-          {
+      <FormRow label="description" error={errors?.description?.message}>
+        <Textarea
+          type="number"
+          id="description"
+          disabled={isWorking}
+          defaultValue=""
+          {...register("description", {
             required: "This field is required",
-          })} />
+          })}
+        />
       </FormRow>
 
       <FormRow label="Cabin Photo">
-        <FileInput id="image" accept="image/*"
-          {...register("image",
-            {
-              required: isEditSession ? false : "This field is required",
-            })} />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: isEditSession ? false : "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow2>
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
-        <Button disabled={isWorking}>{isEditSession ? 'Edit cabin' : 'Create new cabin'}</Button>
+        <Button disabled={isWorking}>
+          {isEditSession ? "Edit cabin" : "Create new cabin"}
+        </Button>
       </FormRow2>
     </Form>
   );
 }
 
 export default CreateCabinForm;
-
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,30 +1,22 @@
-
-import { useState } from "react";
+import AddCabin from "../features/cabins/AddCabin";
 import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
-import Row from "../ui/Row";// import { getCabins } from "../services/apiCabins";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
+import Row from "../ui/Row"; // import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-    const [showForm, setShowForm] = useState(false);
+  return (
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All Cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
 
-    return (
-        <>
-            <Row type="horizontal">
-                <Heading as="h1">All Cabins</Heading>
-                <p>Filter / Sort</p>
-            </Row>
-
-            <Row>
-                <CabinTable />
-
-                <Button onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
-
-                {showForm && <CreateCabinForm />}
-            </Row>
-        </>
-    );
+      <Row>
+        <CabinTable />
+        <AddCabin />
+      </Row>
+    </>
+  );
 }
 
 export default Cabins;

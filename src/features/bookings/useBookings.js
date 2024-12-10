ce54325bcce0e9c7890c32e01679b5159541ae88
Shamilsharaf <165153833+Shamilsharaf@users.noreply.github.com>
2024-12-10 10:15:29 +0530

    API-Side Pagination:Pagination Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -5,27 +5,28 @@ import { useSearchParams } from "react-router-dom";
 export function useBookings() {
   const [searchParams] = useSearchParams();
 
-  //  FILTER
+  // FILTER
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
-  // : { field: "totalPrice", value: 5000, method: "gte" };
 
   // SORT
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  const {
-    isLoading,
-    data: bookings,
-    error,
-  } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, error, bookings };
+  const bookings = data?.data || [];
+  const count = data?.count || 0;
+
+  return { isLoading, error, bookings, count };
 }

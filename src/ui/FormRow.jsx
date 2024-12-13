041d056db93acc58714766f46df50d7f82039a41
Shamilsharaf <165153833+Shamilsharaf@users.noreply.github.com>
2024-12-13 09:42:36 +0530

    Building the signup form

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 function FormRow({ label, error, children }) {
-    return (
-        <StyledFormRow>
-            {label && <Label htmlFor={children.props.id}>{label}</Label>}
-            {children}
-            {error && <Error>{error}</Error>}
-        </StyledFormRow>
-    );
+  return (
+    <StyledFormRow>
+      {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+    </StyledFormRow>
+  );
 }
 
 export default FormRow;

ece77e84eb32ab7614877855493c0dc32e349c33
Shamil C <165153833+Shamilsharaf@users.noreply.github.com>
2025-02-04 21:58:34 +0530

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user1@gmail.com");
+  const [password, setPassword] = useState("user1@123");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
